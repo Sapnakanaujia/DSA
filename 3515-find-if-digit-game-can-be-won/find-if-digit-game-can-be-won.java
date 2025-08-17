48b89ea8cd40b9df319a7d639539c9bedb40d5ce
Sapnakanaujia <sapnakanaujia37@gmail.com>
2025-08-17 23:48:03 +0530

    Time: 1 ms (57.53%) | Memory: 44 MB (93.25%) - LeetSync

--- 3515-find-if-digit-game-can-be-won/find-if-digit-game-can-be-won.java
+++ 3515-find-if-digit-game-can-be-won/find-if-digit-game-can-be-won.java
@@ -2,12 +2,13 @@ class Solution {
     public boolean canAliceWin(int[] nums) {
         int sum1=0;
         int sum2=0;
-           for (int num : nums) {
-            if (num >= 1 && num <= 9) {        // 1-digit
-                sum1 += num;
-            } else if (num >= 10 && num <= 99) { // 2-digit
-                sum2 += num;
+           for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= 1 && nums[i] <= 9) {   // 1 to 9
+                sum1 += nums[i];
+            } else if (nums[i] >= 10 && nums[i] <= 99) { // 10 to 99
+                sum2 += nums[i];
             }
+            // agar 100+ ya negative hua toh skip ho jaayega
         }
          if(sum1!=sum2) return true;
          return false;
