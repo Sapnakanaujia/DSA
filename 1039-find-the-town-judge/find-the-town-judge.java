43ca384bc738aa15084bd0d515b718f6d3956cc9
Sapnakanaujia <sapnakanaujia37@gmail.com>
2025-10-28 16:53:53 +0530

    Time: 3 ms (66.43%) | Memory: 54 MB (59.06%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.java
+++ 1039-find-the-town-judge/find-the-town-judge.java
@@ -1,16 +1,15 @@
 class Solution {
     public int findJudge(int n, int[][] arr) {
-      int[] l1= new int[n+1];
-      int[] l2 =new int[n+1];;
+      int[] cnt= new int[n+1];
         for(int[] num:arr){
             int u=num[0];
             int v=num[1];
-            l1[u]++;
-            l2[v]++;
+            cnt[u]--;
+            cnt[v]++;
 
         }
         for(int i=1;i<=n;i++){
-            if(l2[i]==n-1 && l1[i]==0){
+            if(cnt[i]==n-1){
                 return i;
             }
         }
