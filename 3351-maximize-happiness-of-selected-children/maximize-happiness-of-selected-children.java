e8425d554fe95df741251c4d226d20d5351c3763
Sapnakanaujia <sapnakanaujia37@gmail.com>
2025-12-26 00:00:40 +0530

    Time: 42 ms (70.97%) | Memory: 109.1 MB (65.05%) - LeetSync

---
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class Solution {
+    public long maximumHappinessSum(int[] happiness, int k) {
+        Arrays.sort(happiness); // Sort ascending
+        long sum = 0;
+        int n = happiness.length;
+
+        for (int i = 0; i < k; i++) {
+            int current = happiness[n - 1 - i] - i;
+            if (current > 0) {
+                sum += current;
+            }
+        }
+
+        return sum;
+    }
+}
